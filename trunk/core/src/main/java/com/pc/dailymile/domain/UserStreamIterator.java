d0f34e1f60042a1a70ff260370dea90c267e5985
platers.code@gmail.com <platers.code@gmail.com@64db744e-1dd1-11df-9e2b-99333c9bc362>
2012-01-31 00:46:46 +0000

    adding an iterator so that we can pull all entries for a user

---
+++ trunk/core/src/main/java/com/pc/dailymile/domain/UserStreamIterator.java
@@ -0,0 +1,69 @@
+/*
+   Copyright 2012 platers.code
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+package com.pc.dailymile.domain;
+
+import java.util.Iterator;
+
+import com.pc.dailymile.DailyMileClient;
+import com.pc.dailymile.utils.EntryCriteria;
+
+/**
+ * 
+ * @author jplater
+ *
+ */
+public class UserStreamIterator implements Iterator<Entry> {
+
+    private final String username;
+    private final DailyMileClient client;
+    private final EntryCriteria query;
+    private int currentPage = 1;
+    private Iterator<Entry> curPageIter;
+
+    public UserStreamIterator(DailyMileClient client, String username, EntryCriteria query) {
+        this.username = username;
+        this.client = client;
+        this.query = query;
+    }
+        
+    public UserStreamIterator(DailyMileClient client, String username) {
+        this(client, username, null);
+    }
+    
+    
+    public boolean hasNext() {
+        if (curPageIter == null || !curPageIter.hasNext()) {
+            UserStream stream = client.getUserStream(username, currentPage++, query);
+            if (stream.getEntries() != null) {
+                curPageIter = stream.getEntries().iterator();
+            }
+        }
+        
+        return curPageIter != null && curPageIter.hasNext();
+    }
+
+    public Entry next() {
+        if (hasNext()) {
+            return curPageIter.next();
+        }
+        return null;
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+}
