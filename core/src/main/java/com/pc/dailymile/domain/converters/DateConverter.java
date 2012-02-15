ab5599c440b54e7c7fc99e3f1405fcfe2eff0720
platers.code@gmail.com <platers.code@gmail.com@64db744e-1dd1-11df-9e2b-99333c9bc362>
2012-02-15 03:42:39 +0000

    use jodatime format

--- core/src/main/java/com/pc/dailymile/domain/converters/DateConverter.java
+++ core/src/main/java/com/pc/dailymile/domain/converters/DateConverter.java
@@ -17,7 +17,6 @@
 package com.pc.dailymile.domain.converters;
 
 import java.lang.reflect.Type;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.joda.time.format.DateTimeFormatter;
@@ -33,9 +32,6 @@ import com.google.gson.JsonSerializer;
 
 public class DateConverter implements JsonDeserializer<Date>, JsonSerializer<Date> {
 
-    // 2010-12-25 12:15:00
-    private static String SERIALIZATION_FORMAT = "yyyy-MM-dd HH:mm:ss";
-    
     // 2010-12-25T12:15:00Z
     private DateTimeFormatter deserializeFormatter = ISODateTimeFormat.dateTimeNoMillis(); 
     
@@ -44,8 +40,9 @@ public class DateConverter implements JsonDeserializer<Date>, JsonSerializer<Dat
         
         return deserializeFormatter.parseDateTime(json.getAsJsonPrimitive().getAsString()).toDate();
     }
-
+    
     public JsonElement serialize(Date date, Type typeOfT, JsonSerializationContext context) {
-        return new JsonPrimitive(new SimpleDateFormat(SERIALIZATION_FORMAT).format(date));
+        return new JsonPrimitive(deserializeFormatter.print(date.getTime()));
     }
+    
 }
